43e0357d5d043de37c0bf0db81b364ce6d2fbff9
hexaalif <shadullahsad9@gmail.com>
2023-12-15 17:16:02 +0600

    vercel deploy

--- practice_django_templates/settings.py
+++ practice_django_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2b0i1swj7t%8=f&^w0^4@#+y$#%upzrwymak6g4*)j$0mex2te
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', '.now.sh']
 
 
 # Application definition
